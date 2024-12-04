159c4a1312e1d8d4493e7661faa0ee44db865584
denisetruuong <denise.truong1124@gmail.com>
2024-12-04 11:44:24 -0600

    thoughts ts

--- Develop/src/models/Thoughts.ts
+++ Develop/src/models/Thoughts.ts
@@ -1,47 +1,47 @@
-import { Schema, model, type Document } from 'mongoose';
+import { Schema, model, type Document } from "mongoose";
 
-interface ICourse extends Document {
-    name: string,
-    inPerson: boolean,
-    start: Date,
-    end: Date,
-    students: Schema.Types.ObjectId[]
+interface IThought extends Document {
+  name: string;
+  inPerson: boolean;
+  start: Date;
+  end: Date;
+  users: Schema.Types.ObjectId[];
 }
 
-const courseSchema = new Schema<ICourse>(
-    {
-        name: {
-            type: String,
-            required: true,
-        },
-        inPerson: {
-            type: Boolean,
-            default: true,
-        },
-        start: {
-            type: Date,
-            default: Date.now(),
-        },
-        end: {
-            type: Date,
-            // Sets a default value of 12 weeks from now
-            default: () => new Date(+new Date() + 84 * 24 * 60 * 60 * 1000),
-        },
-        students: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'student',
-            },
-        ],
+const thoughtSchema = new Schema<IThought>(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    {
-        toJSON: {
-            virtuals: true,
-        },
-        timestamps: true
+    inPerson: {
+      type: Boolean,
+      default: true,
     },
+    start: {
+      type: Date,
+      default: Date.now(),
+    },
+    end: {
+      type: Date,
+      // Sets a default value of 12 weeks from now
+      default: () => new Date(+new Date() + 84 * 24 * 60 * 60 * 1000),
+    },
+    users: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "user",
+      },
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    timestamps: true,
+  }
 );
 
-const Course = model<ICourse>('Course', courseSchema);
+const Thought = model<IThought>("Thought", thoughtSchema);
 
-export default Course;
+export default Thought;
